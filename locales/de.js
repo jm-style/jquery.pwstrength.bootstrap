208b0d8ea3a42fd502bc8dbcbedc1f3ea9e32f5f
Jean-Marc Mariani <jm-style@users.noreply.github.com>
2016-09-01 09:39:22 +0200

    Create de.js
    German translations

---
+++ locales/de.js
@@ -0,0 +1,15 @@
+{
+	"wordLength": "Ihr Passwort ist zu kurz",
+	"wordNotEmail": "Verwenden Sie nicht Ihre E-Mail als Passwort",
+	"wordSimilarToUsername": "Ihr Passwort kann nicht enthalten Ihren Benutzernamen ",
+	"wordTwoCharacterClasses": "Verwenden Sie unterschiedliche Charakterklassen ",
+	"wordRepetitions": "Zu viele Wiederholungen",
+	"wordSequences": "Ihr Passwort enthält Sequenzen",
+	"errorList": "Fehler:",
+	"veryWeak": "Sehr schwach",
+	"weak": "Schwach",
+	"normal": "Normal",
+	"medium": "Mittel",
+	"strong": "Stark",
+	"veryStrong": "Sehr stark"
+}
